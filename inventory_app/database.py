92e1b4a3da42064ee2e9e36389a370f03526bb1a
winzato <wisardstat@gmail.com>
2024-02-13 16:41:15 +0700

    Update database.py

--- inventory_app/database.py
+++ inventory_app/database.py
@@ -2,17 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from urllib.parse import quote 
 import configparser
-
-# SQLALCHEMY_DATABASE_URL = (
-#     "mssql+pyodbc://sa:Adm1234@localhost:1433/POSDB_SHOP_MAY01"  
-#     "?driver=ODBC+Driver+17+for+SQL+Server"  
-# )
-  
-# SQLALCHEMY_DATABASE_URL = (
-#     "mssql+pyodbc://wisard_pos_mai:!95fC6kt@mssql-2012.chaiyohosting.com:1433/srakraisoft_pos_mai"  
-#     "?driver=ODBC+Driver+17+for+SQL+Server&schema=dbo"  
-# )
-
+ 
 config = configparser.ConfigParser()
 config.read('./inventory_app/config.ini')
 user        = config['DEFAULT']['user']
