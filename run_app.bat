abc7d00c01d3618163accd746be4aa6b4980ecdc
wisardstat <wisardstat@gmail.com>
2024-05-01 22:23:05 +0700

    None update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -1,3 +1,3 @@
-@REM uvicorn inventory_app.main:app --reload
+uvicorn inventory_app.main:app --reload
 
-uvicorn inventory_app.main:app --reload --port 8080 --host 45.32.102.157
+@REM uvicorn inventory_app.main:app --reload --port 8080 --host 45.32.102.157
